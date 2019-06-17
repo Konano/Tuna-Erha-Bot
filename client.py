b213b435da3364f3984c93fc09ddf927c37277c3
Konano <nanoapezlk@gmail.com>
2019-06-17 15:58:21 +0800

    add c/s

---
+++ client.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+# @Author: Konano
+# @Date:   2019-06-16 17:20:10
+# @Last Modified by:   Konano
+# @Last Modified time: 2019-06-17 15:49:42
+
+import crawler
+import json
+import time
+
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+from socket import *
+from threading import Thread
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(lineno)d - %(funcName)s - %(message)s',
+                    # level=logging.INFO)
+                    level=logging.INFO,
+                    filename=config['CLIENT']['logfile'])
+logger = logging.getLogger(__name__)
+
+
+def detect():
+
+    while True:
+
+        messages = []
+
+        try:
+            category_list = ['academic', '7days', 'business', 'poster', 'research']
+            for category in category_list:
+                messages += crawler.detect(config['URL'][category])
+            messages += crawler.detectBoard(config['URL']['board'])
+        except:
+            continue
+
+        for each in messages:
+            each['title'] = each['title'].replace('[','(').replace(']',')')
+            if each['url'][0] == '/':
+                each['url'] = config['URL']['postinfo'] + each['url']
+
+        try:
+            with open('data/postinfo.json', 'r') as file:
+                lastMessages = json.load(file)
+        except:
+            lastMessages = []
+
+        newMessages = []
+
+        for each in messages:
+            if each not in lastMessages:
+                newMessages.append(each)
+
+        logging.info('Messages: %d, New: %d'%(len(messages),len(newMessages)))
+
+        if len(newMessages) > 0 and len(newMessages) < 10:
+            try:
+                clientSocket.send(json.dumps(newMessages).encode('utf8'))
+            except:
+                logging.exception('ConnectionError')
+                return
+
+        if abs(len(messages)-len(lastMessages)) < 10:
+            with open('data/postinfo.json', 'w') as file:
+                json.dump(messages, file)
+        else:
+            logging.warnning('not Save')
+
+        time.sleep(60)
+
+def main():
+
+    global running, clientSocket
+
+    while True:
+        try:
+            clientSocket = socket(AF_INET,SOCK_STREAM)
+            clientSocket.connect((config['CLIENT']['ip'], config['CLIENT'].getint('port')))
+            logging.info('Connect establish!')
+        except:
+            logging.exception('ConnectionError')
+            time.sleep(60)
+            continue
+
+        detect()
+        clientSocket.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+# @Author: Konano
+# @Date:   2019-05-28 14:12:29
+# @Last Modified by:   Konano
+# @Last Modified time: 2019-06-17 15:56:29
+
+import time
+from socket import *
+from threading import Thread, Lock
+
+
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+owner = config['BOT'].getint('owner')
+group = config['BOT'].getint('group')
+
+def update_config():
+    with open('config.ini', 'w') as configFile:
+        config.write(configFile)
+
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(lineno)d - %(funcName)s - %(message)s',
+                    level=logging.INFO)
+                    # level=logging.INFO,
+                    # filename=config['BOT']['log'])
+logger = logging.getLogger(__name__)
+
+
+from telegram.ext import Updater, CommandHandler, Filters
+
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+# import crawler
+import json
+
+with open('data/mute.json', 'r') as file:
+    mute_list = json.load(file)
+logging.info(mute_list)
+
+lock = Lock()
+newMessages = []
+info_UPDATE = False
+
+def info(bot, job):
+
+    global newMessages, info_UPDATE
+
+    if info_UPDATE == False:
+        return
+    info_UPDATE = False
+
+    lock.acquire()
+
+    newMessages = [each for each in newMessages if each['author'] not in mute_list]
+
+    if newMessages == []:
+        return
+
+    logging.info('Detected new messages: ' + str(len(newMessages)))
+    for each in newMessages:
+        bot.send_message(chat_id=group,
+                         text='Info %s\n[%s](%s)' % (each['author'], each['title'], each['url']),
+                         parse_mode='Markdown')
+
+    lock.release()
+
+def mute(bot, update, args):
+
+    if update.message.chat_id != owner and update.message.chat_id != group:
+        return
+    logging.info('\\mute '+json.dumps(args))
+
+    text = ''
+    for each in args:
+        text += each
+    if text == '':
+        bot.send_message(update.message.chat_id, 'Usage: /mute [source]')
+        return
+
+    global mute_list
+    mute_list.append(text)
+
+    with open('data/mute.json', 'w') as file:
+        json.dump(mute_list, file)
+
+    bot.send_message(update.message.chat_id, 'Muted: ' + text)
+
+def unmute(bot, update, args):
+
+    if update.message.chat_id != owner and update.message.chat_id != group:
+        return
+    logging.info('\\unmute '+json.dumps(args))
+
+    text = ''
+    for each in args:
+        text += each
+    if text == '':
+        bot.send_message(update.message.chat_id, 'Usage: /unmute [source]')
+        return
+
+    global mute_list
+    mute_list.remove(text)
+
+    with open('data/mute.json', 'w') as file:
+        json.dump(mute_list, file)
+
+    bot.send_message(update.message.chat_id, 'Unmuted: ' + text)
+
+def setid(bot, update):
+
+    if update.message.chat_id > 0 or update.effective_user.id != owner:
+        return
+    logging.info('\\setid')
+
+    group = update.message.chat_id
+    config['BOT']['group'] = str(group)
+    update_config()
+
+def mute_show(bot, update):
+
+    if update.message.chat_id != owner and update.message.chat_id != group:
+        return
+    logging.info('\\mute_list')
+
+    text = 'Muted list:'
+    for each in mute_list:
+        text += '\n' + each
+
+    bot.send_message(update.message.chat_id, text)
+
+def connectSocket():
+
+    mainSocket = socket(AF_INET,SOCK_STREAM)
+    mainSocket.bind((config['SERVER']['ip'], config['SERVER'].getint('port')))
+    mainSocket.listen(1)
+
+    global serverSocket
+    logging.info('Wait for connection...')
+    serverSocket,destAdr = mainSocket.accept()
+    logging.info('Connect establish!')
+
+    while True:
+        try:
+            msg = serverSocket.recv(65536).decode('utf8')
+            logging.info('Received: %s'%msg)
+            if msg == '':
+                raise
+            lock.acquire()
+            global newMessages, info_UPDATE
+            newMessages   = json.loads(msg[1:])
+            info_UPDATE   = True
+            lock.release()
+
+        except:
+            logging.exception('ConnectionError')
+            serverSocket.close()
+            logging.info('Wait for connection...')
+            serverSocket,destAdr = mainSocket.accept()
+            logging.info('Connect establish!')
+            continue
+
+    serverSocket.close()
+    mainSocket.close()
+
+
+def main():
+
+    if config['BOT'].getboolean('proxy'):
+        updater = Updater(config['BOT']['accesstoken'], request_kwargs={'proxy_url': config['BOT']['socks5']})
+    else:
+        updater = Updater(config['BOT']['accesstoken'])
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('mute', mute, pass_args=True))
+    dp.add_handler(CommandHandler('unmute', unmute, pass_args=True))
+    dp.add_handler(CommandHandler('mute_list', mute_show))
+    dp.add_handler(CommandHandler('setid', setid))
+
+    updater.job_queue.run_repeating(info, interval=10, first=0, context=group)
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    connect = Thread(target=connectSocket)
+    connect.start()
+    main()
