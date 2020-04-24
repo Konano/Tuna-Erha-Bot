9c85178006a3bfaa5d19fe8226a4c3af4d2d96a3
Konano <nanoapezlk@gmail.com>
2020-04-24 18:56:55 +0800

    fixed minor bug

--- server.py
+++ server.py
@@ -149,7 +149,7 @@ def info(context):
                         del today_news[each['source']]
 
         for each in newMessages:
-            text = 'Info %s\n[%s](%s) [(webvpn)](%s)' % (escaped(each['source']), escaped(each['title')], each['url'], webvpn(each['url']))
+            text = 'Info %s\n[%s](%s) [(webvpn)](%s)' % (escaped(each['source']), escaped(each['title']), each['url'], webvpn(each['url']))
             context.bot.send_message(chat_id=channel, text=text, parse_mode='MarkdownV2', disable_web_page_preview=True)
 
         newMessages = [each for each in newMessages if each['source'] not in muted]
