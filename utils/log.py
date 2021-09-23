29c54e2795f19e93080326809de1dd754ff2e07d
Konano <nanoapezlk@gmail.com>
2021-09-24 00:47:57 +0800

    feat(log): differentiate stdout & stderr

--- utils/log.py
+++ utils/log.py
@@ -1,4 +1,6 @@
-import logging, colorlog
+import sys
+import logging
+import colorlog
 from utils.config import config
 
 BASIC_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(lineno)d - %(funcName)s - %(message)s'
@@ -7,16 +9,35 @@ DATE_FORMAT = None
 basic_formatter = logging.Formatter(BASIC_FORMAT, DATE_FORMAT)
 color_formatter = colorlog.ColoredFormatter(COLOR_FORMAT, DATE_FORMAT)
 
-chlr = logging.StreamHandler()
+
+class MaxFilter:
+    def __init__(self, max_level):
+        self.max_level = max_level
+
+    def filter(self, record):
+        if record.levelno <= self.max_level:
+            return True
+
+
+chlr = logging.StreamHandler(stream=sys.stdout)
 chlr.setFormatter(color_formatter)
 chlr.setLevel('INFO')
+chlr.addFilter(MaxFilter(logging.INFO))
+
+ehlr = logging.StreamHandler(stream=sys.stderr)
+ehlr.setFormatter(color_formatter)
+ehlr.setLevel('WARNING')
 
 fhlr = logging.handlers.TimedRotatingFileHandler(
-    config['BOT']['logpath'] + 'server', when='H', interval=1, backupCount=240)
+    config['BOT']['logpath'] + 'server', when='H', interval=1, backupCount=24*7)
 fhlr.setFormatter(basic_formatter)
 fhlr.setLevel('DEBUG')
+
+logger = logging.getLogger()
+logger.setLevel('NOTSET')
+logger.addHandler(fhlr)
 
 logger = logging.getLogger(__name__)
 logger.setLevel('DEBUG')
 logger.addHandler(chlr)
-logger.addHandler(fhlr)
+logger.addHandler(ehlr)
